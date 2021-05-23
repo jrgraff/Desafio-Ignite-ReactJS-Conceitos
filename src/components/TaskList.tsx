bad2f535b311a3009e071d78ca4bbe29b4f14ffa
Jackson Graff <jacksongraff@hotmail.com>
2021-05-23 04:32:15 -0300

    fix errors

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -20,44 +20,35 @@ export function TaskList() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     const id = Math.floor(Math.random() * 136400);
 
-    if (newTaskTitle) {
-      setTasks([...tasks,
-        {
-          id,
-          title: newTaskTitle,
-          isComplete: false,
-        }
-      ])
-    }
+    if (!newTaskTitle) return;
+
+    setTasks([...tasks,
+      {
+        id,
+        title: newTaskTitle,
+        isComplete: false,
+      },
+    ]);
+
+
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const index = tasks.findIndex(task => task.id === id);
+    const changedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete,
+    } : task);
 
-    let completeCheck = true;
-
-    if(tasks[index].isComplete === true) {
-      completeCheck = false;
-    }
-
-    const taskData = {
-      id,
-      title: tasks[index].title,
-      isComplete: completeCheck,
-    }
-
-    tasks.splice(index, 1);
-      
-    setTasks([...tasks, taskData]);
+    setTasks(changedTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-    const index = tasks.findIndex(task => task.id === id);
+    const filteredTasks = tasks.filter(task => task.id !== id);
     
-    tasks.splice(index, 1)
-    setTasks([...tasks])
+    setTasks(filteredTasks)
   }
 
   return (
